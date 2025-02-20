eebd076d5ac35aa846968c992b52d04fcde9dfae
David Girou <david.girou@0Davgi0.com>
2025-02-20 16:45:41 +0100

    chore: Update version to BETA v0.11.0

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -11,7 +11,7 @@
           <ion-title 
             size="large"
             id="watteco-title">
-              Watteco Easy Codec - BETA v0.10.0
+              Watteco Easy Codec - BETA v0.11.0
           </ion-title>
         </ion-toolbar>
       </ion-header>
