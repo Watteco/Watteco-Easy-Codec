9b67b46251a5722685ac963026808cc4cf0b4d17
0Davgi0 <davgi.wifeo@gmail.com>
2026-05-29 09:20:09 +0200

    fix: update version number to BETA v0.11.11 in TabsPage.vue

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -11,7 +11,7 @@
           <ion-title 
             size="large"
             id="watteco-title">
-              Easy Codec - BETA v0.11.10
+              Easy Codec - BETA v0.11.11
           </ion-title>
         </ion-toolbar>
       </ion-header>
